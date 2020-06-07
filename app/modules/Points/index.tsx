4872cab9f3039557124eeec9669623f132e9d3a8
Daniel Jorge <djpgonzaga@gmail.com>
2020-06-07 19:58:53 -0300

    feature(map): add react native maps, points component with function map, marker and display items

--- app/modules/Points/index.tsx
+++ app/modules/Points/index.tsx
@@ -1,10 +1,215 @@
 import React from 'react';
-import { View } from 'react-native';
-
-// import { Container } from './styles';
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView, Image } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import Constants from 'expo-constants';
+import { Feather as Icon } from '@expo/vector-icons'
+import MapView, { Marker } from 'react-native-maps';
+import { SvgUri } from 'react-native-svg';
 
 const Points: React.FC = () => {
-  return <View />;
+  const navigation = useNavigation();
+
+
+  function handleNavigationBack() {
+    navigation.goBack();
+  }
+
+  function handleNavigationDetail() {
+    navigation.navigate('Details')
+  }
+
+  return (
+    <>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleNavigationBack}>
+          <Icon name="arrow-left" size={20} color="#34cb79" />
+        </TouchableOpacity>
+
+        <Text style={styles.title}>Bem vindo.</Text>
+        <Text style={styles.description}>Encontre no mapa um ponto de coleta.</Text>
+
+        <View style={styles.mapContainer}>
+          <MapView
+            style={styles.map}
+            initialRegion={{
+              latitude: -19.8661054,
+              longitude: -43.9913397,
+              latitudeDelta: 0.014,
+              longitudeDelta: 0.014,
+            }}>
+            <Marker
+              style={styles.mapMarker}
+              onPress={handleNavigationDetail}
+              coordinate={{
+                latitude: -19.8661054,
+                longitude: -43.9913397,
+              }} >
+              <View style={styles.mapMarkerContainer}>
+                <Image
+                  style={styles.mapMarkerImage}
+                  source={{ uri: "https://images.unsplash.com/photo-1583258292688-d0213dc5a3a8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60" }} />
+                <Text style={styles.mapMarkerTitle}> Mercado </Text>
+              </View>
+            </Marker>
+          </MapView>
+        </View>
+      </View>
+      <View style={styles.itemsContainer}>
+        <ScrollView
+          horizontal
+          contentContainerStyle={{ paddingHorizontal: 30 }}
+          showsVerticalScrollIndicator={false}>
+          <TouchableOpacity
+            style={styles.item}
+            onPress={() => { }}>
+            <SvgUri
+              width={42}
+              height={42}
+              uri={'http://192.168.0.56:3333/uploads/lampadas.svg'}>
+            </SvgUri>
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.item}
+            onPress={() => { }}>
+            <SvgUri
+              width={42}
+              height={42}
+              uri={'http://192.168.0.56:3333/uploads/lampadas.svg'}>
+            </SvgUri>
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.item}
+            onPress={() => { }}>
+            <SvgUri
+              width={42}
+              height={42}
+              uri={'http://192.168.0.56:3333/uploads/lampadas.svg'}>
+            </SvgUri>
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.item}
+            onPress={() => { }}>
+            <SvgUri
+              width={42}
+              height={42}
+              uri={'http://192.168.0.56:3333/uploads/lampadas.svg'}>
+            </SvgUri>
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.item}
+            onPress={() => { }}>
+            <SvgUri
+              width={42}
+              height={42}
+              uri={'http://192.168.0.56:3333/uploads/lampadas.svg'}>
+            </SvgUri>
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+        </ScrollView>
+      </View>
+    </>
+  );
 }
 
 export default Points;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 32,
+    paddingTop: 20 + Constants.statusBarHeight,
+  },
+
+  title: {
+    fontSize: 20,
+    fontFamily: 'Ubuntu_700Bold',
+    marginTop: 24,
+  },
+
+  description: {
+    color: '#6C6C80',
+    fontSize: 16,
+    marginTop: 4,
+    fontFamily: 'Roboto_400Regular',
+  },
+
+  mapContainer: {
+    flex: 1,
+    width: '100%',
+    borderRadius: 10,
+    overflow: 'hidden',
+    marginTop: 16,
+  },
+
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+
+  mapMarker: {
+    width: 90,
+    height: 80,
+  },
+
+  mapMarkerContainer: {
+    width: 90,
+    height: 70,
+    backgroundColor: '#34CB79',
+    flexDirection: 'column',
+    borderRadius: 8,
+    overflow: 'hidden',
+    alignItems: 'center'
+  },
+
+  mapMarkerImage: {
+    width: 90,
+    height: 45,
+    resizeMode: 'cover',
+  },
+
+  mapMarkerTitle: {
+    flex: 1,
+    fontFamily: 'Roboto_400Regular',
+    color: '#FFF',
+    fontSize: 13,
+    lineHeight: 23,
+  },
+
+  itemsContainer: {
+    flexDirection: 'row',
+    marginTop: 16,
+    marginBottom: 32,
+  },
+
+  item: {
+    backgroundColor: '#fff',
+    borderWidth: 2,
+    borderColor: '#eee',
+    height: 120,
+    width: 120,
+    borderRadius: 8,
+    paddingHorizontal: 16,
+    paddingTop: 20,
+    paddingBottom: 16,
+    marginRight: 8,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+
+    textAlign: 'center',
+  },
+
+  selectedItem: {
+    borderColor: '#34CB79',
+    borderWidth: 2,
+  },
+
+  itemTitle: {
+    fontFamily: 'Roboto_400Regular',
+    textAlign: 'center',
+    fontSize: 13,
+  },
+});
