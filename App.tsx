08f92cfbabee0eee03a39ef0b49ae4a186c3461c
Daniel Jorge <djpgonzaga@gmail.com>
2020-06-17 19:22:39 -0300

    fix(background-img): add new image_url att

--- App.tsx
+++ App.tsx
@@ -7,11 +7,7 @@ import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
 import Routes from './app/main/routes'
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    Roboto_400Regular,
-    Roboto_500Medium,
-    Ubuntu_700Bold
-  });
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Ubuntu_700Bold });
 
   if (!fontsLoaded) {
     return <AppLoading />
